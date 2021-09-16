f9559e373d1fce4f1151c579caffe5e9325f2cfc
Aditya75117 <90863792+Aditya75117@users.noreply.github.com>
2021-09-16 23:29:49 +0530

    Add files via upload

---
+++ first-api.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "this is api data";
+
+    //include "partials/_dbconnet.php";
+    //"start": "ng serve",
+
+    //extract($_POST);
+
+   // if(isset($_POST['name'])){
+   //     $return_arr = array("msg" => "this is php api",
+   //     "number" => "12");
+   //     echo json_encode($return_arr);
+       
+   // }
+   
+
+?>
